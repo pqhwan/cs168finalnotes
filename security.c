03690c1b18fe877649c8b246581ccbee900fca80
pete <pete@pete-Pangolin-Performance.(none)>
2013-12-29 03:24:56 -0500

    winter 2013 list added

--- security.c
+++ security.c
@@ -1,2 +1,28 @@
 
 Notes for Security 
+
+Eavesdropping- Message Interception (confidentiality)
+
+Tampering- intercepting and modifying the message in the middle
+
+Fabrication- fabricating a message that was never generated
+
+Attack on availability- corrupt packet in trasit, destroy hardware, modify software
+	Denial of Service
+
+Symmetric key encryption 
+
+Asymmetric encryption (public key)
+
+cryptographic hashes
+
+public key authentication: prove you can decipher this!
+
+digital signatures (non repudation)
+
+Public Key Infrastructure
+	Certificate Authority
+
+HTTPS (SSL/TLS)
+
+
---
+++ stack.c
@@ -0,0 +1,31 @@
+
+$Events
+	Mom and Eunice - W hotel
+	Eunice and Woody
+	Bukchon
+	Eunice, junsang and his gf, me
+	Nice night out!
+
+	Hyunkyu
+	Uncle
+	Brown 2015 meeting 2012
+	1/12-1/13 go skiing
+
+$issues
+	Wifi additional router
+	Classes next semester
+	Brown hackathon
+
+$topics
+	iOS dev
+	java-multithreading
+	js-Backbone
+	Structure and interpretation of Computer Programs
+	Cracking the coding interview
+
+$interests
+	Gravity
+	Walter Mitty ~~
+	Stop Smoking
+	AB music theory - no point if not exercise, really 
+
